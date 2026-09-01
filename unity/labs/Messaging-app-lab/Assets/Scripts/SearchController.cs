45639199e3e62450706519b1149a2e7a52128b23
djou <julia.sangnakk@hes-so.ch>
2026-09-01 15:05:57 +0200

    searchBar arrow next/prev

--- unity/labs/Messaging-app-lab/Assets/Scripts/SearchController.cs
+++ unity/labs/Messaging-app-lab/Assets/Scripts/SearchController.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SearchController : MonoBehaviour
 {
+    [Header("Search")]
     [SerializeField] private TMP_Text searchText;
     [SerializeField] private string wordToType = "MEMENTO VIVERE";
     [SerializeField] private float typingSpeed = 0.06f;
 
+    [Header("Results")]
+    [SerializeField] private TMP_Text counterText;
+    [SerializeField] private RectTransform[] results;
+
+    [Header("Scroll")]
+    [SerializeField] private ScrollRect scrollRect;
+
+    private int currentResult = 0;
+
     public void StartSearch()
     {
+        currentResult = 0;
+        UpdateCounter();
         StartCoroutine(TypeSearch());
     }
 
@@ -22,5 +35,31 @@ public class SearchController : MonoBehaviour
             searchText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
+    }
+
+    public void NextResult()
+    {
+        if (currentResult < results.Length - 1)
+        {
+            currentResult++;
+        }
+
+        UpdateCounter();
+    }
+
+    public void PreviousResult()
+    {
+        if (currentResult > 0)
+        {
+            currentResult--;
+    }
+
+        UpdateCounter();
+    }
+   
+
+    private void UpdateCounter()
+    {
+        counterText.text = (currentResult + 1) + "/" + results.Length;
     }
 }
