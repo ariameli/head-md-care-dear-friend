5bc4ad6f68ca51f2ab9454a43017267e36300f73
djou <julia.sangnakk@hes-so.ch>
2026-09-21 23:40:44 +0200

    Eugenie folder content opens

--- unity/DearFriend/Assets/Scripts/ObjectAppears.cs
+++ unity/DearFriend/Assets/Scripts/ObjectAppears.cs
@@ -9,14 +9,18 @@ public class ObjectAppears : MonoBehaviour
    public string dialogueNodeName;
    public GameObject objectToActivate;
     
-[YarnCommand("activateObject")]
-   public void activeateObject()
-   {   
-        // activate the specified object
-        if (objectToActivate != null)
-        {
-            objectToActivate.SetActive(true);
-        }
+   [YarnCommand("activateObject")]
+   public static void ActivateObject(string objectName)
+   {
+      foreach (GameObject sceneObject in Resources.FindObjectsOfTypeAll<GameObject>())
+      {
+         if (sceneObject.name == objectName && sceneObject.scene.IsValid())
+         {
+            sceneObject.SetActive(true);
+            return;
+         }
+      }
 
+      Debug.LogWarning($"ObjectAppears: could not find scene object '{objectName}'.");
    }
 }
